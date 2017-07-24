ea10ca461eba4271d8c0b48d9be2901e6dce7e7a
Qata <24601033+q-ata@users.noreply.github.com>
2017-07-24 17:54:39 -0400

    rip token

--- src/main/java/main/Constants.java
+++ src/main/java/main/Constants.java
@@ -2,7 +2,7 @@ package main;
 
 public class Constants {
   
-	public static final String TOKEN = "MzM2NjM3MDcyNzM3Njk3Nzkz.DFEs5Q.mwKndsWRzAvh-MB24Qy4milAkWs";
+	public static final String TOKEN = "[REDACTED]";
 	
 	public static final String PREFIX = "-";
 	

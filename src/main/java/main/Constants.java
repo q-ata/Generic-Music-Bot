5a56da981a32f4bb3c2c6f536bfc3a49d7f7936a
Qata <24601033+q-ata@users.noreply.github.com>
2017-07-24 21:21:31 -0400

    Delete Constants.java

--- src/main/java/main/Constants.java
+++
@@ -1,9 +0,0 @@
-package main;
-
-public class Constants {
-  
-	public static final String TOKEN = "MzM2NjM3MDcyNzM3Njk3Nzkz.DFf-MA.0YINmhdL0NyfhNhC2nbeHV2GMVI";
-	
-	public static final String PREFIX = "-";
-	
-}

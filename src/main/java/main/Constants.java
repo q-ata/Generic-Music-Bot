1e92df6ca0a6cc3a3d132ae82ca445ab325933db
Qata <george.smithx00@gmail.com>
2017-07-24 21:26:56 -0400

    a

--- src/main/java/main/Constants.java
+++
@@ -1,9 +0,0 @@
-package main;
-
-public class Constants {
-  
-	public static final String TOKEN = "MzM2NjM3MDcyNzM3Njk3Nzkz.DFgtTQ.GyMFnvrTMC3i4yOV9PERt-nTeFs";
-	
-	public static final String PREFIX = "-";
-	
-}
